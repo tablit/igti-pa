dba841d714d92ef13fd47503622efd73c57ce943
tablit <tata.angelo@gmail.com>
2023-12-17 16:20:05 -0300

    Adding basic data exploration

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # igti-pa
 Repositório para projeto aplicado da pós de engenharia de dados do igti
+
+# Criação de virtualenv
+
+python -m venv /path/to/new/virtual/environment
+
+source env/bin/activate
+
+pip install poetry
+
+poetry completions bash >> ~/.bash_completion
+
+poetry run python your_script.py
+
+
--- read_csv_data.py
+++ read_csv_data.py
@@ -2,3 +2,20 @@
 # [x] Manually download data and upload to folder 
 # [] read csv as pd.DataFrame
 # [] find example FI - CNPJ: 68.670.512/0001-07
+
+import pandas as pd
+
+# url: https://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/
+daily_report = pd.read_csv("data/if_daily_report/202312/inf_diario_fi_202312.csv",sep=";")
+
+# captação do dia - CAPT_DIA
+# cnpj do fundo - CNPJ_FUNDO 
+# data competencia - DT_COMPT
+# numero de cotistas - NR_COTST
+# resgate do dia - RESG_DIA
+# tipo do fundo - TP_FUNDO
+# patrimônio líquido - VL_PATRIM_LIQ
+# valor da cora - VL_QUOTA
+# valor total da carteira - VL_TOTAL
+
+csn_daily_report = daily_report.loc[daily_report['CNPJ_FUNDO']=='68.670.512/0001-07']
